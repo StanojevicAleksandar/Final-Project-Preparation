b9c3eab930abe737d4b578b58dfd012f07b2eefb
StanojevicAleksandar <109697885+StanojevicAleksandar@users.noreply.github.com>
2022-08-01 22:00:45 +0200

    edit user.js

--- user.js
+++ user.js
@@ -1,12 +1,23 @@
-const postListEl = document.querySelector (".post-list");
+const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
 
-async function main() {
+function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id)
+
+}
+
+async function renderPosts(id) {
     const id = localStorage.getItem("id")
-    const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
     console.log(postsData)
 
-    postLisEl.innerHTML = postsData.map(post => `
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join("");
+}
+
+function postHTML(post) {
+    return `
     <div class="post">
       <div class="post__title">
         ${post.title}
@@ -15,7 +26,7 @@ async function main() {
         ${post.body}
       </p>
     </div>
-    `).join("");
+    `
 }
 
-main();
+renderPosts(id);
